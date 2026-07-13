81ebdff5f9c17a3c4fb109996d6bf165b5213b70
sairamgundu16 <sairam.gundu24@sasi.ac.in>
2026-07-13 14:42:43 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dns =  require("dns")
+dns.setServers([["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/ContactManager");
+        await mongoose.connect("mongodb+srv://sairamgundu24:<db_password>@cluster0.6gru7la.mongodb.net/sairam");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
